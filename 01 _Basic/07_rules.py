987661d4ec8132efc0852120e5b95ea7cf8a98af
tomarcodinglife <tomarcodinglife@gmail.com>
2024-12-18 09:28:11 +0530

    updated

---
+++ 01 _Basic/07_rules.py
@@ -0,0 +1,8 @@
+# Rules
+
+print('''RULE
+1. A variable name can content alphabet, digits, and underscores
+2. A variable name can only start with an alphabet and undersores
+3. A variable name cant't start with a digit
+4. No while space is allowed to be use inside a variable name
+''')
