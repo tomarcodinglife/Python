9db327802c22f1486b56b7764b46a5a40dfdc6fb
tomarcodinglife <tomarcodinglife@gmail.com>
2024-12-19 21:22:20 +0530

    updated

---
+++ 01 _Basic/13_escapeSequence.py
@@ -0,0 +1,15 @@
+# Escape Sequence
+
+# \n – Newline
+# \t – Tab
+# \\ – Backslash
+# \' – Single quote
+# \" – Double quote
+# \r – Carriage return
+# \b – Backspace
+# \f – Form feed
+# \v – Vertical tab
+# \u / \U – Unicode character
+# \x – Hexadecimal value
+
+a = "i am Sujit Tomar from "
