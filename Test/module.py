699eb0d4ebe05265be1ead502d1abe465dd366fa
tomarcodinglife <tomarcodinglife@gmail.com>
2024-12-19 20:41:12 +0530

    updated

--- Test/module.py
+++ Test/module.py
@@ -1,5 +1,6 @@
 import pyttsx3
 word = "i am Sujit Kumar Singh from Aurangabad Bihar 'Thank You So Much'"
+print(word)
 textToSpeech = pyttsx3.init()
 textToSpeech.say(word)
 textToSpeech.runAndWait()
