2b88d4b594cdf98e110d94541d7b826b8ff242dc
tomarcodinglife <tomarcodinglife@gmail.com>
2024-12-21 09:07:08 +0530

    updated

--- 01 _Basic/13_escapeSequence.py
+++ 01 _Basic/13_escapeSequence.py
@@ -1,15 +1,56 @@
 # Escape Sequence
 
-# \n – Newline
-# \t – Tab
-# \\ – Backslash
-# \' – Single quote
-# \" – Double quote
-# \r – Carriage return
-# \b – Backspace
-# \f – Form feed
-# \v – Vertical tab
-# \u / \U – Unicode character
-# \x – Hexadecimal value
+# 1. \n – Newline
+print("---------------------Newline---------------------") 
+name = "Hello \n World"
+print(name) 
 
-a = "i am Sujit Tomar from "
+# 2. \t – Tab (horizontal tab)
+print("---------------------horizontal tab---------------------") 
+address = "151 Jordan garden, \t Turonto, Brajil - 122455"
+print(address)
+
+
+# 3. \\ – Backslash
+print("---------------------Backslash---------------------") 
+information = "antani is the best guy \\ in the world."
+print(information)
+
+# 4. \' – Single quote (useful in single-quoted strings)
+print("---------------------Single quote---------------------") 
+language = "\'Python is one of the best programming language in computer languge\'"
+print(language)
+
+# 5. \" – Double quote (useful in double-quoted strings)
+print("---------------------Double quote---------------------") 
+quotes = "\"We are not what we know but what we are willing to learn.\""
+print(quotes)
+
+# 6. \r – Carriage return (returns the cursor to the beginning of the line)
+print("--------------------- Carriage return ---------------------")
+fullName = "Sujit \r tomar"
+print(fullName)
+
+# 7. \b – Backspace (removes the previous character)
+print("--------------------- Backspace ---------------------")
+favLanguageLine = "Hello \b World"
+print(favLanguageLine)
+
+# 8. \f – Form feed
+print("--------------------- Form feed ---------------------")
+
+formFeed = "Hello World \f This is the next page."
+print(formFeed)
+
+# 8. \v – Vertical tab
+print("--------------------- Vertical tab ---------------------")
+lessonSecond =  "Hello World \v This is below the vertical tab."
+print(lessonSecond)
+# 9. \u / \U – Unicode character
+mathsymbol = "a \u03A9 b"
+# print(mathsymbol)
+
+# 10. \x – Hexadecimal value  (Character represented by a hexadecimal value)
+alphabetLetter = "\x41"
+
+# 11. print(alphabetLetter)
