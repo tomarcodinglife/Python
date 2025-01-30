9d48f1ac9cadccdd821ccd4179a270ca4c7beec4
tomarcodinglife <tomarcodinglife@gmail.com>
2025-01-30 21:35:30 +0530

    updated

--- Test/35_test.py
+++ Test/35_test.py
@@ -2,7 +2,7 @@
 # celsius = 5 X (fahrenheit-32)/9
 
 def f_to_c(f):
-    print(f"{(f-32)*5/9}° Celsius")
+    print(round((f-32)*5/9), 2) # round(value, Number of decimal)
     return((f-32)*5/9)
 
 fahrenheit = int(input("Enter Fahrenheit Number for Convert in Cesius: "))
