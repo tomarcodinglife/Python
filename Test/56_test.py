9e74fe394451100deb6f54b6500fbf3444853471
tomarcodinglife <tomarcodinglife@gmail.com>
2025-02-06 07:25:56 +0530

    updated

--- Test/56_test.py
+++ Test/56_test.py
@@ -1,7 +1,7 @@
-with open("C:/Users/PC/OneDrive/Documents/Python/57_textFile01.txt", "r") as file01:
+with open("C:/Users/PC/OneDrive/Documents/Python/Test/57_textFile01.txt", "r") as file01:
     content01 = file01.read()
 
-with open("C:/Users/PC/OneDrive/Documents/Python/58_textFile02.txt", "r") as file02:
+with open("C:/Users/PC/OneDrive/Documents/Python/Test/58_textFile02.txt", "r") as file02:
     content02 = file02.read()
 
 if(content01 == content02):
