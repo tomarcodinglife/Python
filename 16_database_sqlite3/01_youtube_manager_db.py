e389864b83b77de6249fce7d61689c063b96134a
tomarcodinglife <tomarcodinglife@gmail.com>
2025-02-17 09:33:26 +0530

    updated

--- 16_database_sqlite3/01_youtube_manager_db.py
+++ 16_database_sqlite3/01_youtube_manager_db.py
@@ -20,13 +20,19 @@ def list_video():
 def add_video():
     pass
 
+def update_video():
+    pass
+
+def delete_video():
+    pass
+
 def main():
     while True:
         print("\n Youtube Manager app with DB")
         print("1. List Video")
         print("2. Add Video")
         print("3. Update Video")
-        print("5. Delete Video")
+        print("4. Delete Video")
         print("5. Exit App")
         choice = input("Enter your choice: ")
 
@@ -37,10 +43,20 @@ def main():
             duration = input("Enter the video Duration: ")
             add_video(name, duration)
         elif choice == "3":
-            input("Enter video ID to updated ")
+            video_id = input("Enter video ID to updated: ")
             name = input("Enter the video Name: ")
             duration = input("Enter the video Duration: ")
-            add_video(name, duration)
+            update_video(video_id, name, duration)
+        elif choice == "4":
+            video_id = input("Enter video ID to delete: ")
+            name = input("Enter the video Name: ")
+            duration = input("Enter the video Duration: ")
+            delete_video(video_id)
+        elif choice == "5":
+            break
+        else:
+            print("Invalid Choice ")
+
 
 if __name__ == "__main__":
     main()
